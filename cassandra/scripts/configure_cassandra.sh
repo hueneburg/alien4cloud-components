b381c374a2991429ad524b232cb2000f21103820
Armin Hüneburg <hueneburg.armin@gmail.com>
2018-06-19 13:44:24 +0200

    Scaling
    Signed-off-by: Armin Hüneburg <hueneburg.armin@gmail.com>

--- cassandra/scripts/configure_cassandra.sh
+++ cassandra/scripts/configure_cassandra.sh
@@ -1,1 +1,9 @@
 #! /bin/bash
+
+CLUSTER=${clustername}
+
+sudo /etc/init.d/cassandra stop
+sed -i "s/Test Cluster/${CLUSTER}/g" /etc/cassandra/cassandra.yaml
+sed -i "s/SimpleSnitch/GossipingPropertyFileSnitch/g" /etc/cassandra/cassandra.yaml
+sed -i "s/127.0.0.1/$(hostname -i)/g" /etc/cassandra/cassandra.yaml
+
--- cassandra/scripts/scale_up_cassandra.sh
+++ cassandra/scripts/scale_up_cassandra.sh
@@ -1,1 +1,11 @@
 #! /bin/bash
+
+CLUSTER=${clustername}
+ENTRY=${entrypoint}
+
+sudo /etc/init.d/cassandra stop
+sed -i "s/Test Cluster/${CLUSTER}/g" /etc/cassandra/cassandra.yaml
+sed -i "s/SimpleSnitch/GossipingPropertyFileSnitch/g" /etc/cassandra/cassandra.yaml
+sed -i "s/127.0.0.1/$(hostname -i)/g" /etc/cassandra/cassandra.yaml
+sed -i "s/- seeds: .*/- seeds: \"$(hostname -i),${ENTRY}\"" /etc/cassandra/cassandra.yaml
+
