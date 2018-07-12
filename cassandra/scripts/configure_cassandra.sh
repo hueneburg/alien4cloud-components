191140987f0e3c22609b76d3de3928c156ea62ba
Armin Hüneburg <hueneburg.armin@gmail.com>
2018-07-12 07:59:16 +0200

    debug
    Signed-off-by: Armin Hüneburg <hueneburg.armin@gmail.com>

--- cassandra/scripts/configure_cassandra.sh
+++ cassandra/scripts/configure_cassandra.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+echo test > ~/armin_install
+
 CLUSTER=${CLUSTER_NAME}
 
 hostname=$(hostname -i | awk '{ print $1 }')
@@ -10,8 +12,19 @@ sudo rm -rf /var/lib/cassandra/data/*
 # Clean logs
 sudo rm -rf /var/log/cassandra/gc* /var/log/cassandra/system.log
 
-echo "CONFIGURING" >> log
 sudo sed -i -E "s/(Cassandra[ -])?Test[ -]Cluster/${CLUSTER}/gI" /etc/cassandra/cassandra.yaml
 sudo sed -i "s/SimpleSnitch/GossipingPropertyFileSnitch/g" /etc/cassandra/cassandra.yaml
 sudo sed -i "s/127.0.0.1/${hostname}/g" /etc/cassandra/cassandra.yaml
 sudo sed -i "s/localhost/${hostname}/g" /etc/cassandra/cassandra.yaml
+
+echo configuring > ~/armin_install
+
+ENTRY=${ENTRYPOINT}
+
+if [[ -z ${ENTRY} ]]; then
+  exit
+fi
+
+hostname=$(hostname -i | awk '{ print $1 }')
+
+sudo sed -i -E "s/- seeds: .*/- seeds: \"${ENTRY}\,${hostname}"/" /etc/cassandra/cassandra.yaml
